5727b2e6e6fb18d0feef365726d33e1c036254d5
DanyYanev <yanev89@gmail.com>
2018-05-12 22:43:15 +0300

    added login pages

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,22 @@
+class LoginController < ApplicationController
+
+    #GET /login
+    def index
+
+    end
+
+    #GET /register
+    def register
+
+    end
+
+    #GET /login/reset
+    def reset
+
+    end
+
+    #GET /login/forgot
+    def forgot
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   get '/hot' => 'posts#hot'
   get '/fresh' => 'posts#fresh'
+  get '/login' => 'login#index'
+  get '/register' => 'login#register'
+  get '/login/reset' => 'login#reset'
+  get '/login/forgot' => 'login#forgot'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
